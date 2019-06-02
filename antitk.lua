8eb5a9db07069919d3338645c3b60d2dc655a407
Dobytchick <48864677+Dobytchick@users.noreply.github.com>
2019-06-02 19:50:01 +0600

    Anti-teamkill

---
+++ antitk.lua
@@ -0,0 +1,22 @@
+PLUGIN.name = "Anti-teamkill"
+PLUGIN.author = "Dobytchick"
+PLUGIN.desc = "Notify and kick for teamkilling player."
+
+function PLUGIN:PlayerSpawn(client)
+    client:setNetVar("rdm_counter", 0)
+end
+
+function PLUGIN:PlayerDeath(client, inflictor, attacker)
+    if client != attacker and IsValid(client) and IsValid(attacker) and attacker:IsPlayer() then
+        if attacker:Team() == client:Team() then
+			attacker:setNetVar("rdm_counter", attacker:getNetVar("rdm_counter",0) + 1)
+			attacker:notify("Вы убили своего, если вы продолжите - вы получите бан")
+        end
+    end
+
+    if (client:getChar()) then
+        if attacker:getNetVar("rdm_counter",0) > 2 then 
+            attacker:Ban(60,true) 
+        end 
+    end
+end

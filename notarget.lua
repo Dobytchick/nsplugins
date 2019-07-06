8dc0906d8277ea5a2693a2abbe49d1f7b04b573e
Dobytchick <48864677+Dobytchick@users.noreply.github.com>
2019-07-06 17:30:10 +0600

    Update notarget.lua

--- notarget.lua
+++ notarget.lua
@@ -1,12 +1,15 @@
-PLUGIN.name = "Нотаргит"
+PLUGIN.name = "VJBASE Notarget"
 PLUGIN.author = "Dobytchick"
-PLUGIN.desc = "Хуяргит блять"
+PLUGIN.desc = "Notrarget for NPC on vjbase"
 
 if SERVER then
-    function PLUGIN:PlayerLoadedChar(client, character, lastChar)
-        if client:Team() == FACTION_MONOLIT then
-            client:SetNoTarget(true) 
-            client.VJ_NoTarget = true
+    function PLUGIN:PlayerSpawn(player)
+        if player:Team() == FACTION_MONOLIT then
+            player:SetNoTarget(true) 
+            player.VJ_NoTarget = true
+        else
+            player:SetNoTarget(false) 
+            player.VJ_NoTarget = false
         end
     end
 end

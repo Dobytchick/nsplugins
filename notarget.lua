5d03ad75dfd02513defe62c998012cc3d0886891
Dobytchick <48864677+Dobytchick@users.noreply.github.com>
2019-08-06 14:03:49 +0600

    Simplified adding fractions to the table
    Notrarget for NPC on vjbase

--- notarget.lua
+++ notarget.lua
@@ -2,9 +2,17 @@ PLUGIN.name = "VJBASE Notarget"
 PLUGIN.author = "Dobytchick"
 PLUGIN.desc = "Notrarget for NPC on vjbase"
 
+local factions = {}
+
+local function addTable(text)
+    factions[text] = true
+end
+
+addTable(FACTION_MONOLIT) -- Use this function for add faction on notarget.
+
 if SERVER then
     function PLUGIN:PlayerSpawn(player)
-        if player:Team() == FACTION_MONOLIT then
+        if factions[player:Team()] then
             player:SetNoTarget(true) 
             player.VJ_NoTarget = true
         else

e295ca6c16dfc137055a1b9c0a1d7e9dbfd262ab
Dobytchick <48864677+Dobytchick@users.noreply.github.com>
2019-08-26 23:05:16 +0600

    updated steamid verification method in blacklist

--- blacklist.lua
+++ blacklist.lua
@@ -1,21 +1,21 @@
 PLUGIN.name = "Blacklist for bad guys"
-PLUGIN.author = "Dobytchick"
+PLUGIN.author = "Dobytchick, Carlsmei"
 PLUGIN.desc = "This plugin dds a blacklist. Do not remove those who are in it, because they can harm your servers"
 
 local blacklist = {}
 
 local function add_to_bk(key,rsn)
-    blacklist[key] = {reason = rsn}
+    blacklist[key] = {rsn}
 end
 
 --add_to_bk(string,string)
 add_to_bk("STEAM_0:1:155659578", "damage to the server")
 
 if SERVER then
-    function PLUGIN:PlayerAuthed(ply, steamid, uniqueid)
-    if blacklist[ply:SteamID()] then
-        ply:Kick("Вы добавлены в черный список на данном сервере. Причина:"..blacklist[ply:SteamID()].reason)
-        ply:Ban(0, false)
+    function PLUGIN:CheckPassword(steamid64)
+        local steamid = util.SteamIDFrom64(steamid64)
+        if blacklist[steamid] then
+            return false, blacklist[steamid]
+        end
     end
-end
 end

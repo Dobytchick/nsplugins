30c9256c35f8dd3e1070f4ee4e2a36aa941dbd52
Taxin2012 <mrtaxin2012@mail.ru>
2019-11-16 17:18:43 +0300

    Some fixes
    1) Useless function
    2) Banning the not valid player

--- blacklist.lua
+++ blacklist.lua
@@ -2,20 +2,14 @@ PLUGIN.name = "Blacklist for bad guys"
 PLUGIN.author = "Dobytchick, Carlsmei"
 PLUGIN.desc = "This plugin adds a blacklist"
 
-local blacklist = {}
-
-local function add_to_bk(key,rsn)
-    blacklist[key] = {rsn}
-end
-
---add_to_bk(string,string)
-add_to_bk("STEAM_0:1:155659578", "damage to the server")
+local bList = {}
+bList[ "STEAM_0:1:155659578" ] = "Damage to the server"
 
 if SERVER then
-    function PLUGIN:PlayerAuthed(ply, steamid, uniqueid)
-        if blacklist[ply:SteamID()] then
-            ply:Kick("Вы добавлены в черный список на данном сервере. Причина:"..blacklist[ply:SteamID()])
-            ply:Ban(0, false)
+    function PLUGIN:PlayerAuthed( ply, steamid, uniqueid )
+        if blacklist[ ply:SteamID() ] then
+            ply:Ban( 0 )
+            ply:Kick( "Вы добавлены в черный список на данном сервере. Причина:" .. blacklist[ ply:SteamID() ] )
         end
     end
 end

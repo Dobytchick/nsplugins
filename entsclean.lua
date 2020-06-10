34ea1c65f09005a563bb901b58f3bfcefe8e3a8c
Dobytchick <48864677+Dobytchick@users.noreply.github.com>
2020-06-10 08:57:44 +0600

    Update entsclean.lua

--- entsclean.lua
+++ entsclean.lua
@@ -2,23 +2,28 @@ PLUGIN.name = "Misc ents remove"
 PLUGIN.author = "Dobytchick"
 PLUGIN.desc = "Remove ragdolls, items and other shit."
 
-local text = "After 60 seconds all items will be deleted (Ragdolls, items, money)"
+PLUGIN.textToClean = "Через 60 секунд удалятся предметы, регдоллы, деньги, лежащие на земле"
 
-local EntsForClean = {}
-EntsForClean["prop_ragdoll"] = true
-EntsForClean["nut_item"] = true
-EntsForClean["nut_money"] = true
+PLUGIN.EntsForClean = {}
+PLUGIN.EntsForClean["prop_ragdoll"] = true
+PLUGIN.EntsForClean["nut_item"] = true
+PLUGIN.EntsForClean["nut_money"] = true
+
+nut.config.add("cleanTime", 700, "Seconds before garbage disposal", nil, {
+	data = {min = 1, max = 1000},
+	category = "Misc Ents Remove"
+})
 
 if CLIENT then
-    timer.Create("notify_1_minute", 640, 0, function()
-        for i=1,4 do
-            chat.AddText(Color(217, 89, 89),text)
+    timer.Create("notify_1_minute", nut.config.get("cleanTime") - 60, 0, function()
+        for i=1,2 do
+            chat.AddText(Color(217, 89, 89),"[!] "..textToClean)
         end
     end)
 end
 
 if SERVER then
-    timer.Create("Timer", 700, 0, function()
+    timer.Create("Timer", nut.config.get("cleanTime"), 0, function()
         for k,v in pairs(ents.GetAll()) do
             if EntsForClean[v:GetClass()] then
                 v:Remove()

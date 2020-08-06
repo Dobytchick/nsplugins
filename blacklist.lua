96af0266afd3448f6b76575fb30aca5944f9d248
Dobytchick <48864677+Dobytchick@users.noreply.github.com>
2020-08-07 00:16:38 +0600

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -3,11 +3,11 @@ PLUGIN.author = "Dobytchick, Carlsmei"
 PLUGIN.desc = "This plugin adds a blacklist"
 
 local bList = {}
-bList[ "STEAM_0:1:155659578" ] = "Damage to the server"
+bList["STEAM_0:1:155659578"] = "Damage to the server"
 
 if SERVER then
     function PLUGIN:PlayerAuthed( ply, steamid, uniqueid )
-        if blacklist[ ply:SteamID() ] then
+        if blacklist[ply:SteamID()] then
             ply:Ban( 0 )
             ply:Kick( "Вы добавлены в черный список на данном сервере. Причина:" .. blacklist[ ply:SteamID() ] )
         end

65d53ed1e112204e9de7832a86a97acc8766d11b
Dobytchick <48864677+Dobytchick@users.noreply.github.com>
2021-02-23 01:58:19 +0600

    Update antitk.lua

--- antitk.lua
+++ antitk.lua
@@ -2,33 +2,31 @@ PLUGIN.name = "Anti-teamkill"
 PLUGIN.author = "Dobytchick"
 PLUGIN.desc = "Notify and kick for teamkilling player."
 
-function PLUGIN:PlayerSpawn(client)
-    client:setNetVar("rdm_counter", 0)
-end
+local set = cookie.Set
+local get = cookie.GetNumber
 
 function PLUGIN:PlayerDeath(client, inflictor, attacker)
     if client != attacker and IsValid(client) and IsValid(attacker) and attacker:IsPlayer() then
         if attacker:Team() == client:Team() then
-		attacker:setNetVar("killed_players", attacker:getNetVar("killed_players",0) + 1)
-		attacker:notify("You killed your, if you continue - you will get a ban!")
+		attacker.attack_counter = (attacker.attack_counter || 0) + 1
+			
+		if attacker.attack_counter > 1 then
+			set("rdm_count"..attacker:SteamID64(), attacker.attack_counter)
+			attacker:notify("You killed your, if you continue - you will get a ban!")
+		end
+			
+		timer.Simple(10,function() 
+			if attacker.attack_counter then 
+				attacker.attack_counter = attacker.attack_counter - 1 
+				set("rdm_count"..attacker:SteamID64(), attacker.attack_counter)
+			end 
+		end)
         end
     end
-	
-	timer.Create("CheckForKills"..attacker:EntIndex(),300,0,function()
-		if attacker:getNetVar("killed_players",0) != 0 then
-			attacker:setNetVar("killed_players", attacker:getNetVar("killed_players",0) - 1)
-		end
-	end)
 
     if (client:getChar()) then
-        if attacker:getNetVar("killed_players",0) > 2 then 
+        if get("rdm_count"..attacker:SteamID64(), 0) > 2 then 
             attacker:Ban(60,true) 
         end 
-    end
-end
-
-function PLUGIN:PlayerDisconnected(ply)
-    if timer.Exists("CheckForKills"..ply:EntIndex()) then
-        timer:Remove()
     end
 end
